20f4b9c74ccccf75c27d211c141de77d06ac04b5
BiancaTS <53481713+BiancaTS@users.noreply.github.com>
2023-10-30 11:26:14 -0300

    Operadores Compostos
    Teste de código em Python. Operadores Compostos. Exercício de faculdade de Ciência da Computação. Paradígmas de Linguagem de Programação em Python - Python Básico - Características das Variáveis em Python.

---
+++ Operadores Compostos.py
@@ -0,0 +1,6 @@
+x = 10
+print("x={}".format(x))
+x+=2
+print("x={}".format(x))
+x-=2
+print("x={}".format(x))
---
+++ Operadores Compostos02.py
@@ -0,0 +1,7 @@
+x = 10
+x*=2
+print("x={}".format(x))
+x/=2
+print("x={}".format(x))
+x%=2
+print("x={}".format(x))
